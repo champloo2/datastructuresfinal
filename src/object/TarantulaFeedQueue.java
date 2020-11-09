bce0a2cf1687be6ebb8276e6da458b031c555d32
unknown <ebeth.luong@gmail.com>
2020-11-08 22:50:24 -0600

    created class to track feeding and sales

---
+++ src/object/TarantulaFeedQueue.java
@@ -0,0 +1,23 @@
+package object;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class TarantulaFeedQueue {
+	
+	Queue<Tarantula> feedQueue = new LinkedList<Tarantula>(); 
+	Stack<Tarantula> stack = new Stack<Tarantula>();
+	
+	//method to add a spider to feed
+	
+	//method to feed spider
+	//once spider is fed, add it to sell stack
+	
+	//be able to move tarantulas stack back to queue
+	
+	//be able to make a sale
+	
+	//maybe re-make this class renamed
+
+}
